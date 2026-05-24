d21165558ee45b9c1b2efab4e92814d5c3221a9b
hawraakt <hak001@usal.ed.lb>
2026-05-24 17:31:11 +0300

    16

---
+++ Multi_Store.Core/Entities/Coupon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Store.Core.Entities
+{
+    public class Coupon
+    {
+        // Primary Key
+        private int couponID;
+
+        // Foreign Key (Nullable)
+        private int? storeID;
+
+        // Attributes
+        private string couponCode;
+        private string discountType;
+        private decimal discountValue;
+        private decimal minimumOrderAmount;
+        private decimal maximumDiscountAmount;
+
+        private DateTime startDate;
+        private DateTime endDate;
+
+        private int usageLimit;
+        private int usagePerCustomerLimit;
+        private int usedCount;
+
+        private bool isActive;
+
+        // Relationships
+
+        // Many coupons can belong to one store
+        private Store store;
+    }
+}
---
+++ Multi_Store.Core/Entities/OrderItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Store.Core.Entities
+{
+    public class OrderItem
+    {
+        // Primary Key
+        private int orderItemID;
+
+        // Foreign Keys
+        private int orderID;
+        private int productID;
+        private int storeID;
+
+        // Attributes
+        private string productName;
+
+        private decimal productPrice;
+
+        private int quantity;
+
+        private decimal totalPrice;
+
+        private bool reviewSubmitted;
+
+        // Relationships
+
+        // Many OrderItems belong to one Order
+        private Order order;
+
+        // Many OrderItems reference one Product
+        private Product product;
+
+        // Many OrderItems belong to one Store
+        private Store store;
+    }
+}
