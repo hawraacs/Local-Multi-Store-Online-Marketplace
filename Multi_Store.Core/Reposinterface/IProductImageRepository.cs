17a240a07322a6e1bd243fc5dd5a6323b3f9e2cd
hawraakt <hak001@usal.ed.lb>
2026-05-26 23:26:19 +0300

    repo

---
+++ Multi_Store.Core/Reposinterface/IProductImageRepository.cs
@@ -0,0 +1,19 @@
+﻿using Multi_Store.Core.Entities;
+using Multi_Store.Core.Reposinterface.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Store.Core.Reposinterface
+{
+    public interface IProductImageRepository : IRepository<ProductImage>
+    {
+        Task<IReadOnlyList<ProductImage>> GetByProductAsync(int productId);
+
+        Task<ProductImage?> GetPrimaryImageAsync(int productId);
+
+        Task<int> GetMaxDisplayOrderAsync(int productId);
+    }
+}
