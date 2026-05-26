738608368be726b30d4f9caf752110e4aa266520
hawraakt <hak001@usal.ed.lb>
2026-05-27 01:32:06 +0300

    repo finish

---
+++ Multi_Store.Core/Reposinterface/IUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Multi_Store.Core.Entities;
+using Multi_Store.Core.Reposinterface.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Store.Core.Reposinterface
+{
+
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User?> GetByEmailAsync(string email);
+
+        Task<User?> GetByPhoneAsync(string phoneNumber);
+
+        Task<IReadOnlyList<User>> GetByRoleAsync(int roleId);
+
+        Task<IReadOnlyList<User>> GetActiveUsersAsync();
+
+        Task<bool> EmailExistsAsync(string email);
+
+        Task<bool> PhoneExistsAsync(string phoneNumber);
+    }
+}
