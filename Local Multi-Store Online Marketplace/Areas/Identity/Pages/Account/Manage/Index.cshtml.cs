8dfd57c7bc40d1ccfe815aa86c21453792f45e85
hawraakt <hak001@usal.ed.lb>
2026-05-28 15:07:32 +0300

    e

--- Local Multi-Store Online Marketplace/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Local Multi-Store Online Marketplace/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -9,17 +9,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Multi_Store.Core.Entities;
 
 namespace Local_Multi_Store_Online_Marketplace.Areas.Identity.Pages.Account.Manage
 {
     public class IndexModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
         public IndexModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -60,7 +61,7 @@ namespace Local_Multi_Store_Online_Marketplace.Areas.Identity.Pages.Account.Mana
             public string PhoneNumber { get; set; }
         }
 
-        private async Task LoadAsync(IdentityUser user)
+        private async Task LoadAsync(User user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
