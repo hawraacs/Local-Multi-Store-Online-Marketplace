5cf374f537bd968b1a3ef3ee287d9f5f41ab7311
hawraakt <hak001@usal.ed.lb>
2026-05-31 04:33:31 +0300

    customer

---
+++ Local Multi-Store Online Marketplace/Pages/CustomerProducts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Local_Multi_Store_Online_Marketplace.Pages
+{
+    public class CustomerProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
