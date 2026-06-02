3cd792698bbf7f5a0aac2fa44203b1c522d2ad4f
rawan dbouk <rhd161@usal.edu.lb>
2026-06-02 12:15:37 +0300

    .

--- Multi-Store.Infrastructure/Migrations/20260601191532_InitialCreate.cs
+++ Multi-Store.Infrastructure/Migrations/20260601191532_InitialCreate.cs
@@ -4,18 +4,32 @@
 
 namespace Multi_Store.Infrastructure.Migrations
 {
+    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
+        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.RenameColumn(
                 name: "ImageURL",
                 table: "ProductImages",
                 newName: "ImageUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "ProductImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
         }
 
+        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "ProductImages");
+
             migrationBuilder.RenameColumn(
                 name: "ImageUrl",
                 table: "ProductImages",
