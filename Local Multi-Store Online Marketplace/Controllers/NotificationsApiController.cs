0d72ecd7fe2a1832c8f7789a2dd15c14459778fe
rawan dbouk <rhd161@usal.edu.lb>
2026-07-11 03:18:57 +0300

    notification

---
+++ Local Multi-Store Online Marketplace/Controllers/NotificationsApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multi_Store.Core.Entities;
+using Multi_Store.Services.Managers;
+
+namespace Local_Multi_Store_Online_Marketplace.Controllers
+{
+    [ApiController]
+    [Route("api/notifications")]
+    [Authorize]
+    public class NotificationsApiController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly NotificationManager _notificationManager;
+
+        public NotificationsApiController(
+            UserManager<User> userManager,
+            NotificationManager notificationManager)
+        {
+            _userManager = userManager;
+            _notificationManager = notificationManager;
+        }
+
+        [HttpPost("mark-messages-read")]
+        public async Task<IActionResult> MarkMessagesRead()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var unread = await _notificationManager.GetUnreadAsync(user.Id);
+
+            foreach (var n in unread.Where(x => x.Type == "Message"))
+            {
+                await _notificationManager.MarkAsReadAsync(n.NotificationID);
+            }
+
+            return Ok(new { success = true });
+        }
+    }
+}
