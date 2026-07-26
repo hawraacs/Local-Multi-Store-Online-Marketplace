2711bd41553e9f5d2bd4e413d8c134c93a6278b1
rawan dbouk <rhd161@usal.edu.lb>
2026-07-27 00:39:10 +0300

    .

---
+++ Multi-Store.Infrastructure/Migrations/20260726211346_AddIsHiddenByDeliveryPerson.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Multi_Store.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsHiddenByDeliveryPerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatMessages_Products_ProductID",
+                table: "ChatMessages");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHiddenByDeliveryPerson",
+                table: "DeliveryAssignments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatMessages_Products_ProductID",
+                table: "ChatMessages",
+                column: "ProductID",
+                principalTable: "Products",
+                principalColumn: "ProductID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatMessages_Products_ProductID",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "IsHiddenByDeliveryPerson",
+                table: "DeliveryAssignments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatMessages_Products_ProductID",
+                table: "ChatMessages",
+                column: "ProductID",
+                principalTable: "Products",
+                principalColumn: "ProductID",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
